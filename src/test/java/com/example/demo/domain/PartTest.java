ebe7a8e993297e7bcdb1adfd743b78728616e1eb
sprab <tspraba@wgu.edu>
2025-08-25 15:52:23 -0500

    Part i  added tests to PartTest.java

--- src/test/java/com/example/demo/domain/PartTest.java
+++ src/test/java/com/example/demo/domain/PartTest.java
@@ -156,4 +156,39 @@ class PartTest {
         partOut.setId(1l);
         assertEquals(partIn.hashCode(),partOut.hashCode());
     }
+
+    @Test
+    void getMinInv(){
+        int minInv=1;
+        partIn.setMinInv(minInv);
+        assertEquals(minInv,partIn.getMinInv());
+        partOut.setMinInv(minInv);
+        assertEquals(minInv,partOut.getMinInv());
+    }
+    @Test
+    void setMinInv() {
+        int minInv = 1;
+        partIn.setMinInv(minInv);
+        assertEquals(minInv, partIn.getMinInv());
+        partOut.setMinInv(minInv);
+        assertEquals(minInv, partOut.getMinInv());
+    }
+
+    @Test
+    void getMaxInv(){
+        int maxInv=100;
+        partIn.setMaxInv(maxInv);
+        assertEquals(maxInv,partIn.getMaxInv());
+        partOut.setMaxInv(maxInv);
+        assertEquals(maxInv,partOut.getMaxInv());
+    }
+
+    @Test
+    void setMaxInv(){
+        int maxInv=100;
+        partIn.setMaxInv(maxInv);
+        assertEquals(maxInv,partIn.getMaxInv());
+        partOut.setMaxInv(maxInv);
+        assertEquals(maxInv,partOut.getMaxInv());
+    }
 }
